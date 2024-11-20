9828dca0d9c3d74f2c9f33fffc4b5df2703477dc
Kleverson Costa <kleverson.mmc@gmail.com>
2024-11-20 12:56:55 -0300

    validates URL from form

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,46 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1>Welcome</h1>
-      <MyButton />
+      <h1>Tá perdido fi?</h1>
+      <URLForm />
     </div>
   )
 }
 
-function MyButton() {
+function URLForm() {
+  const isValidURL = (url: string) => {
+    try {
+      new URL(url);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const url = formData.get('url') as string;
+    if (!isValidURL(url)) {
+      alert('URL inválida!')
+    }
+
+    const requiresConfirmation = formData.get('requiresConfirmation') == 'on';
+    console.log(url, requiresConfirmation);
+  };
+
   return (
-    <button>BOTÃO</button>
+    <form className='inputForm' onSubmit={handleSubmit}>
+      <label className='inputLabel'>
+        Endereço URL:
+        <input type="text" name="url" placeholder="https://exemplo.com" required/>
+        </label>
+        <label className='inputLabel'>
+          Exibir página de confirmação antes de redirecionar
+          <input type='checkbox' id='confirmCheckbox' name='requiresConfirmation' />
+        </label>
+      <input type="submit" value="ENCURTAR" className='prettyButtom'/>
+    </form>
   )
 }
 
