4b637e2825c14943531b72068765ae9a71533cd7
Kleverson Costa <kleverson.mmc@gmail.com>
2024-11-21 11:19:19 -0300

    corrects deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Jabalinks-frontend/',
   server: {
     watch: {
       usePolling: true,
